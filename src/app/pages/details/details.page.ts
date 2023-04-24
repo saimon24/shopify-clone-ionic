0cbbbdc5e9f354134d079c1b613f00df3fd5fea8
Simon Grimm <saimon@devdactic.com>
2023-04-24 09:51:50 +0200

    Update values and gesture to work on native devices.

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -17,6 +17,7 @@ import {
   IonRouterOutlet,
   IonicModule,
   Platform,
+  isPlatform,
 } from '@ionic/angular';
 
 @Component({
@@ -32,14 +33,15 @@ export class DetailsPage implements OnInit, AfterViewInit {
   @ViewChild('myDiv') myDiv!: ElementRef;
   @ViewChild('dropdownBtn') dropdownBtn!: ElementRef;
   gesture?: Gesture;
-  divBottom: number = -170;
+  divBottomPosition: number = -170;
+  tabsHeight: number = isPlatform('ios') ? 51 : 56;
 
   constructor(
     private routerOutlet: IonRouterOutlet,
     private gestureCtrl: GestureController,
     private domCtrl: DomController,
     private renderer: Renderer2,
-    private platform:Platform,
+    private platform: Platform
   ) {
     this.presentingElement = this.routerOutlet.nativeEl;
   }
@@ -55,53 +57,86 @@ export class DetailsPage implements OnInit, AfterViewInit {
   }
 
   createGesture() {
-    this.gesture = this.gestureCtrl.create({
-      el: this.myDiv.nativeElement,
-      threshold: 0,
-      gestureName: 'my-gesture',
-      onMove: ev => this.onMoveHandler(ev),
-      onStart: () => this.onStartEnd(),
-      onEnd: () => this.onStartEnd(true)
-    });
-    this.gesture.enable()
+    this.gesture = this.gestureCtrl.create(
+      {
+        el: this.myDiv.nativeElement,
+        threshold: 0,
+        gestureName: 'my-gesture',
+        onMove: (ev) => this.onMoveHandler(ev),
+        onStart: () => this.onStartHandler(),
+        onEnd: () => this.onStartEnd(true),
+      },
+      true
+    );
+    this.gesture.enable();
   }
 
-  private onStartEnd(shouldEnableTransition:boolean = false) {
-    this.renderer.setStyle(this.myDiv.nativeElement, '--transition-duration', `${shouldEnableTransition ? 300 : 0}ms`, RendererStyleFlags2.DashCase);
-    this.divBottom = this.getDivBottom();
-
-    // The following should work, I feel like the logic makes sense but somehow it just doesn't. It Almost works on web but on the device it completely breaks
-    // if bottom sheet is not fully closed
-    if(this.divBottom >= -170 && this.divBottom <  -85) return this.writeStyle(-170);
-    // if bottom sheet is not fully opened
-    if(this.divBottom <= 0 && this.divBottom >= -85) return this.writeStyle(0);
-  }
-
-  private getDivBottom() {
-    // the height of the view minus height of the tab-bar minus the bottom of the div
-    return this.platform.height() - 57 - this.myDiv.nativeElement.getBoundingClientRect().bottom;
+  onStartHandler() {
+    this.renderer.setStyle(
+      this.myDiv.nativeElement,
+      '--transition-duration',
+      '0',
+      RendererStyleFlags2.DashCase
+    );
   }
 
   onMoveHandler(ev: GestureDetail) {
     let bottom: number = this.getBottom(ev.deltaY);
-    this.renderer.setStyle(this.myDiv.nativeElement, '--transition-duration', '0', RendererStyleFlags2.DashCase);
-    this.domCtrl.write(() => this.writeStyle(bottom) );
+    this.domCtrl.write(() => this.writeStyle(bottom));
+  }
+
+  private onStartEnd(shouldEnableTransition: boolean = false) {
+    this.renderer.setStyle(
+      this.myDiv.nativeElement,
+      '--transition-duration',
+      `${shouldEnableTransition ? 300 : 0}ms`,
+      RendererStyleFlags2.DashCase
+    );
+    this.divBottomPosition = this.getDivBottom();
+
+    if (this.divBottomPosition > -85) {
+      this.domCtrl.write(() => this.writeStyle(0));
+    } else {
+      this.domCtrl.write(() => this.writeStyle(-170));
+    }
   }
 
   private writeStyle(bottom: number) {
-    this.renderer.setStyle(this.dropdownBtn.nativeElement, 'rotate', `${bottom > -85 ? 180 : 0}deg`);
-    this.renderer.setStyle(this.myDiv.nativeElement, 'bottom', `${bottom}px`)
+    this.renderer.setStyle(
+      this.dropdownBtn.nativeElement,
+      'rotate',
+      `${bottom > -90 ? 180 : 0}deg`
+    );
+    this.renderer.setStyle(this.myDiv.nativeElement, 'bottom', `${bottom}px`);
+  }
+
+  toggleBottomSheet() {
+    this.domCtrl.write(() => {
+      this.renderer.setStyle(
+        this.myDiv.nativeElement,
+        'bottom',
+        `${this.getDivBottom() < -90 ? 0 : -170}px`
+      );
+      this.renderer.setStyle(
+        this.dropdownBtn.nativeElement,
+        'rotate',
+        `${this.getDivBottom() < -90 ? -180 : 0}deg`
+      );
+    });
+  }
+
+  private getDivBottom() {
+    return (
+      this.platform.height() -
+      this.tabsHeight -
+      this.myDiv.nativeElement.getBoundingClientRect().bottom
+    );
   }
 
   private getBottom(deltaY: number) {
-    let bottom = this.divBottom - deltaY;
-    if (bottom > 0) return bottom = 0;
-    if(bottom < -170 ) return -170;
+    let bottom = this.divBottomPosition - deltaY;
+    if (bottom > 0) return (bottom = 0);
+    if (bottom < -170) return -170;
     return bottom;
-  }
-
-  toggleBottomSheet() {1
-    this.renderer.setStyle(this.myDiv.nativeElement, 'bottom', `${this.getDivBottom() < -85 ? 0 : -170}px`);
-    this.renderer.setStyle(this.dropdownBtn.nativeElement, 'rotate', `${this.getBottom(0)}deg`);
   }
 }
